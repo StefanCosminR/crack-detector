980576eaca3481ac0ea89a699f0ef4a9d50976a8
stefancosmin <stefancosmin.romanescu@outlook.com>
2019-10-27 21:53:03 +0200

    Added another image processor

---
+++ CommandLineApp/CrackDetection/ImageProcessorV2.py
@@ -0,0 +1,99 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow import keras
+from keras_preprocessing.image import ImageDataGenerator
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
+
+batch_size = 32
+IMG_HEIGHT = 227
+IMG_WIDTH = 227
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_dir = os.path.join(r'/Users/stefancosmin/Faculty/IP/DataSet1')
+
+train_image_generator = keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=45,
+    width_shift_range=.15,
+    height_shift_range=.15,
+    horizontal_flip=True,
+    zoom_range=0.5,
+    validation_split=0.2
+)  # Generator for our training data)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                           directory=train_dir,
+                                                           shuffle=False,
+                                                           color_mode="grayscale",
+                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           class_mode='binary')
+
+sample_training_images, _ = next(train_data_gen)
+
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+# plotImages(sample_training_images[:5])
+
+model = Sequential([
+    Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 1)),
+    MaxPooling2D(),
+    # Conv2D(32, 3, padding='same', activation='relu'),
+    # MaxPooling2D(),
+    # Conv2D(64, 3, padding='same', activation='relu'),
+    # MaxPooling2D(),
+    Flatten(),
+    Dense(2048, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+# model.fit(
+#     train_data_gen,
+#     steps_per_epoch= 40000 // batch_size,
+#     epochs=10
+# )
+
+history = model.fit_generator(
+    train_data_gen,
+    #steps_per_epoch= // batch_size,
+    epochs=10
+)
+
+acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+# val_loss = history.history['val_loss']
+
+epochs_range = range(10)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
