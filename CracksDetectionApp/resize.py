3d899d0acbb25053383128491dbd92b5a799d221
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-12-01 16:27:25 +0200

    Added new algorithm for crack detection

---
+++ CracksDetectionApp/resize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import os
+
+from PIL import Image
+
+path = "images"
+output_path = "images_resized"
+dirs = os.listdir(path)
+
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path + item):
+            im = Image.open(path + item)
+            f, e = os.path.splitext(output_path + item)
+            im_resize = im.resize((128, 128), Image.ANTIALIAS)
+            im_resize.save(f + '.jpg', 'JPEG', quality=99)
+
+
+resize()
